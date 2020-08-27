07c311afb79e7992351ba71de0b4a7b153c1e0ed
viktoriiashv <viktoriiash@tahometer.com>
2020-08-27 10:12:03 +0300

    Task Controller and Task Model created

---
+++ TodoList/Controllers/TaskController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoList.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoList.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        // GET: api/tasks
+        [HttpGet("")]
+        public string Get()
+        {
+            return Task.getTaskListJson();
+        }
+
+        // POST api/tasks
+        [HttpPost("")]
+        public void Post(Task task)
+        {
+            Task.addTask(task);
+        }
+    }
+}
---
+++ TodoList/Models/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+
+        public Task()
+        {
+        }
+
+        public Task(int id, string name, bool done)
+        {
+            this.id = id;
+            this.name = name;
+            this.done = done;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool done { get; set; }
+
+        static Dictionary<int, Task> ToDoList = new Dictionary<int, Task>();
+
+        public static void addTask(Task task)
+        {
+            task.id = Task.ToDoList.Count > 0 ? Task.ToDoList.Keys.Max() + 1 : 1; //max needs at least 1 element in dictionary
+            Task.ToDoList.Add(task.id, task);
+        }
+
+        public static string getTaskListJson()
+        {
+            JsonSerializerOptions jso = new JsonSerializerOptions();
+            jso.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            return JsonSerializer.Serialize(Task.ToDoList.Select(d => d.Value), jso);
+        }
+    }
+}
